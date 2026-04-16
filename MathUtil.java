13d2ddb2700423d98b8ed523abe2f6b90c6c41d1
alammdhazrat <alammdhazrat743@gmail.com>
2026-04-16 11:46:27 +0530

    Add logging in add method

--- MathUtil.java
+++ MathUtil.java
@@ -1,8 +1,9 @@
 public class MathUtil {
 
     public static int add(int a, int b) {
-        return a + b;
-    }
+    System.out.println("Adding numbers...");
+    return a + b;
+}
     public static void main(String[] args) {
         int result = add(5, 10);
         System.out.println("The sum is: " + result);
